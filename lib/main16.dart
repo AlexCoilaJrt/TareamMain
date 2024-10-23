8f2b63adbacf03a60e24e409f4fe690d43a0f047
alexanderhuahuacccapacc <alexander123huahuaccapa@gmail.com>
2024-10-23 11:38:33 -0500

    asdasdasd

---
+++ lib/main16.dart
@@ -0,0 +1,52 @@
+
+import 'package:flutter/material.dart';
+void main() {
+  runApp( MyApp());
+}
+final ScrollController _scrollController = ScrollController();
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('ScrollController')),
+      body: ListView.builder(
+        controller: _scrollController,
+        itemCount: 30,
+        itemBuilder: (context, index) {
+          return ListTile(title: Text('Item $index'));
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _scrollController.animateTo(
+            _scrollController.position.maxScrollExtent,
+            duration: Duration(seconds: 2),
+            curve: Curves.easeOut,
+          );
+        },
+        child: Icon(Icons.arrow_downward),
+      ),
+    );
+  }
+
+}
